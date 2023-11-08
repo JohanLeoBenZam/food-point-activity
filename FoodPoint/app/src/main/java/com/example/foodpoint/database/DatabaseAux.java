b64092e8d1684e0aef5fd8f13648ff706b4e3819
JohanLeoBenZam <132547367+JohanLeoBenZam@users.noreply.github.com>
2023-11-08 11:24:16 +0100

    Update DatabaseAux.java

--- FoodPoint/app/src/main/java/com/example/foodpoint/database/DatabaseAux.java
+++ FoodPoint/app/src/main/java/com/example/foodpoint/database/DatabaseAux.java
@@ -24,6 +24,31 @@ public class DatabaseAux extends SQLiteOpenHelper {
                         "userpassword VARCHAR(25) NOT NULL)");
     }
 
+    db.execSQL("CREATE TABLE comidas " +
+        "(idComida INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "tipo VARCHAR(25) NOT NULL," +
+        "restaurante VARCHAR(25) NOT NULL," +
+        "reserva VARCHAR(25) NOT NULL)");
+
+db.execSQL("CREATE TABLE tipo " +
+        "(idTipo INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "nombreTipo VARCHAR(25) NOT NULL)");
+
+db.execSQL("CREATE TABLE restaurante " +
+        "(idRestaurante INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "nombreRestaurante VARCHAR(25) NOT NULL," +
+        "valoracion INTEGER NOT NULL," +
+        "direccion VARCHAR(50) NOT NULL," +
+        "horario VARCHAR(50) NOT NULL," +
+        "telefono VARCHAR(25) NOT NULL)");
+
+db.execSQL("CREATE TABLE reserva " +
+        "(idReserva INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "nombreReserva VARCHAR(25) NOT NULL," +
+        "personas INTEGER NOT NULL," +
+        "hora VARCHAR(25) NOT NULL," +
+        "notaParaRestaurante VARCHAR(200) NOT NULL)");
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
